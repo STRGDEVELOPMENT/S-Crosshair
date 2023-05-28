0a54906cf4bcd970d4fe0cc73f25617418b624ea
STRG <134840814+STRGDEVELOPMENT@users.noreply.github.com>
2023-05-28 20:49:40 +0100

    Add files via upload

---
+++ S-Crosshair/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'STRG'
+description 'A Simple Fivem Crosshair.'
+version '1.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/crosshair.png',
+    'ui/index.html',
+    'ui/script.js',
+    'ui/style.css'
+}
